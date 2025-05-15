2c401db8eccf647fa05091d8b77fdcadaef35f07
juyonLee00 <ljy012900@gmail.com>
2025-05-15 23:58:05 +0900

    [Chore] #1 Edit variable name

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    private float speed = 50f;
-    private Vector3 rotateAxis = Vector3.up;
+    private float _rotationSpeed = 50f;
+    private Vector3 _rotationAxis = Vector3.up;
     void Update()
     {
-        transform.Rotate(rotateAxis, speed * Time.deltaTime);
+        transform.Rotate(_rotationAxis, _rotationSpeed * Time.deltaTime);
     }
 }
