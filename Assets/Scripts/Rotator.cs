be0ce5b793a4658ba889c912593d671eb88b4fab
juyonLee00 <ljy012900@gmail.com>
2025-05-24 00:45:54 +0900

    [Feat] #1 Modify the rotation axis of target

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
     private float _rotationSpeed = 50f;
-    private Vector3 _rotationAxis = Vector3.up;
+    private Vector3 _rotationAxis = Vector3.back;
     void Update()
     {
         transform.Rotate(_rotationAxis, _rotationSpeed * Time.deltaTime);
