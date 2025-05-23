83fd27c794d5865eb590f3d4bb51fe95dc5e6eed
juyonLee00 <ljy012900@gmail.com>
2025-05-24 00:52:59 +0900

    [Feat] #1 Set the projectile to attach to the target at the desired angle

--- Assets/Scripts/ProjectileSpawner.cs
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -1,12 +1,45 @@
+using System;
 using UnityEngine;
 
 public class ProjectileSpawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject _projectilePrefab;
+    [SerializeField] 
+    private Transform _targetTransform;
+    [SerializeField] 
+    private Vector3 _targetPosition;
+    [SerializeField] 
+    private float _targetRadius;
+    [SerializeField]
+    private float _projectileLength;
+
+    private void Awake()
+    {
+        _targetPosition = Vector3.up * 2;
+        _targetRadius = 1.0f;
+        _projectileLength = 1.5f;
+    }
+    
 
     public void SpawnThrowableProjectile(Vector3 position)
     {
         Instantiate(_projectilePrefab, position, Quaternion.identity);
+    }
+
+    public void SpawnStuckProjectile(float angle)
+    {
+        GameObject cloneProjectile = Instantiate(_projectilePrefab);
+
+        SetInPinProjectileToTarget(cloneProjectile.transform, angle);
+    }
+
+    private void SetInPinProjectileToTarget(Transform projectileTransform, float angle)
+    {
+        projectileTransform.position =
+            Utils.GetPositionFromAngle(_targetRadius + _projectileLength, angle) + _targetPosition;
+        projectileTransform.rotation = Quaternion.Euler(0, 0, angle);
+        projectileTransform.SetParent((_targetTransform));
+        projectileTransform.GetComponent<Pin>().SetInPinProjectileToTarget();
     }
 }
